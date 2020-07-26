ced0a0992f15b1e51aca2b151e08d41e959903d7
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2020-07-26 08:29:51 -0400

    ENH: Over9000 optimizer, flat and annealing scheduler, 10 epochs, lr=0.001, OOF CV: 0.8792 (#23)

--- siim_isic_melanoma_classification/submit.py
+++ siim_isic_melanoma_classification/submit.py
@@ -1,15 +1,14 @@
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 
-from siim_isic_melanoma_classification.constants import (
-    submissions_path,
-    test_fpath,
-)
+from siim_isic_melanoma_classification.constants import test_fpath
 
 
-def prepare_submission(y_preds: np.ndarray, fname: str):
+def prepare_submission(y_pred: np.ndarray, fpath: Path):
     image_names = pd.read_csv(test_fpath)
     preds_df = pd.DataFrame(
-        {"image_name": image_names["image_name"], "target": y_preds}
+        {"image_name": image_names["image_name"], "target": y_pred}
     )
-    preds_df.to_csv(submissions_path / fname, index=False)
+    preds_df.to_csv(fpath, index=False)
