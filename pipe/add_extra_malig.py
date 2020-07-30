486331808fd39bb70940220428d444708d2cb2b5
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2020-07-29 20:41:09 -0400

    ENH: use external data from 2017 and 2018 competition, OOF CV: 8829

---
+++ pipe/add_extra_malig.py
@@ -0,0 +1,56 @@
+from shutil import copytree
+from zipfile import ZipFile
+
+from siim_isic_melanoma_classification.constants import data_path
+from tensorboard.compat import tf
+import shutil
+import os
+
+szs = ["128", "192", "256", "384", "512"]
+
+
+def main():
+
+    for sz in szs:
+        source_path = data_path / f"train_{sz}"
+        destination_path = data_path / f"train_with_extra_{sz}"
+
+        if destination_path.exists():
+            destination_path.rmdir()
+            destination_path.mkdir()
+
+        # copy original train dataset
+        print(f"Copying {sz}x{sz} training images...")
+        copytree(src=source_path, dst=destination_path)
+
+        # unseen images from ISIC's online gallery
+        print(f"Extracting {sz}x{sz} images from ISIC's online gallery...")
+        archive = ZipFile(f"./malignant-v2-{sz}x{sz}.zip")
+        for f in archive.namelist():
+            if f.startswith("jpeg"):
+                fname = os.path.basename(f)
+                source_to = archive.open(f)
+
+                with open(destination_path / fname, "wb") as target:
+                    shutil.copyfileobj(source_to, target)
+
+        # images from 2017, 2018, and 2019 competitions
+        print(f"Extracting {sz}x{sz} images from 2019 challenge...")
+        archive = ZipFile(f"./jpeg-isic2019-{sz}x{sz}.zip")
+        for f in archive.namelist():
+            if f.endswith(".jpg"):
+                fname = os.path.basename(f)
+                source_to = archive.open(f)
+
+                with open(destination_path / fname, "wb") as target:
+                    shutil.copyfileobj(source_to, target)
+
+    # extract meta-data for these extra images
+    archive = ZipFile("./malignant-v2-128x128.zip")
+    for f in archive.namelist():
+        if f.endswith(".csv"):
+            archive.extract(f, data_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ pipe/split_with_extra.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+from siim_isic_melanoma_classification.constants import (
+    data_path,
+    folds_fpath,
+    folds_with_extra_fpath,
+)
+
+
+def main():
+    original_folds = pd.read_csv(folds_fpath)
+    cols = original_folds.columns
+
+    new_malig = [
+        pd.read_csv(data_path / f"train_malig_{n}.csv") for n in range(1, 4)
+    ]
+    new_malig = pd.concat(new_malig, axis=0)
+
+    # split malignant cases from 2017 and 2018 competition
+    # between 5 folds
+    condition = [
+        new_malig.tfrecord.isin([30, 32, 34]),
+        new_malig.tfrecord.isin([36, 38, 40]),
+        new_malig.tfrecord.isin([42, 44, 46]),
+        new_malig.tfrecord.isin([48, 50, 52]),
+        new_malig.tfrecord.isin([54, 56, 58]),
+    ]
+    choice = [0, 1, 2, 3, 4]
+    new_malig["fold"] = np.select(condition, choice, default=-1)
+
+    # remove extra images and cols we don't want yet
+    new_malig = new_malig[new_malig.fold != -1]
+    new_malig = new_malig.loc[
+        :, cols
+    ]  # TODO: introduce extra features for meta-model
+
+    folds_with_extra = pd.concat([original_folds, new_malig], axis=0)
+    folds_with_extra.to_csv(folds_with_extra_fpath, index=False)
+
+
+if __name__ == "__main__":
+    main()
